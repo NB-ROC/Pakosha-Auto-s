0b171da32d9d32c711860cae729bf81bde9a7be1
Ryzex <Ryzexgamer1@gmail.com>
2025-11-03 14:15:48 +0100

    testtttt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,13 +19,7 @@
 <body>
   @include('nav.header')
 
-  <main class="site-main container" style="padding:28px 16px;">
-    <h1>hello</h1>
 
-    <form action="">
-      <button>login</button>
-    </form>
-  </main>
 
   @include('nav.footer')
 </body>
